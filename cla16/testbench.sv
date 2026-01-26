185437b5a6291a26b47cbb97f35c9c6a39b2010b
Jko2012 <julianohern@gmail.com>
2026-01-26 11:12:52 -0600

    Updated testbench

--- cla16/testbench.sv
+++ cla16/testbench.sv
@@ -1,13 +1,15 @@
 module testbench();
-	logic [15:0] a, b, s, c;
+	logic [15:0] a, b, s;
 	logic cin, cout;
 	//instatiate device under test
-	cla16 dut(a, b, cin, s, c, cout);
+	cla16 dut(a, b, cin, s, cout);
 	//apply test inputs
 	initial begin
 		a = 0;		b = 0; 		cin = 0; 	#10;
 		a = 20;  	b = 21;			    		#10;
 		a = 65534;  b = 1; 		cin = 1; 	#10;
+						b = 2;						#10;
+		a = 65535;	b = 65535;					#10;
 		a = 387;		b = 1234;	cin = 0;		#10;
 		a = 10;		b = 54; 						#10;
 		a = 29084;  b = 2090;	cin = 1;		#10;
