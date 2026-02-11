0dead937e8a67ac9c78d39421e0ea05061c3421a
Dalton Reynolds <123430215+DaltonPReynolds@users.noreply.github.com>
2026-02-10 18:19:00 -0600

    Refactor adder and subtractor modules in SignMag17
    swapped add and diff

--- SignMag17/SignMag17.sv
+++ SignMag17/SignMag17.sv
@@ -8,8 +8,8 @@ endmodule
 
 module subtractor(
     input logic [15:0] a,b,
-    output logic [15:0] diff,
-    input logic add,
+	output logic [15:0] add,
+    input logic diff,
     output logic OF);
 
     logic addnot;
@@ -228,3 +228,4 @@ assign OF_D = OF;
 assign S = {SnSum,MSum};
 assign OF_S = Cout;
 endmodule
+
