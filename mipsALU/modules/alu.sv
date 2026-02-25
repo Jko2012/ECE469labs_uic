a8c67238e8c0358728736877bf92e565e8c14242
Jko2012 <julianohern@gmail.com>
2026-02-25 13:05:29 -0600

    moved Tessa's changes to alu.sv

--- mipsALU/modules/alu.sv
+++ mipsALU/modules/alu.sv
@@ -1,17 +1,18 @@
 module alu(
-	input		logic	[31:0]	A, B,
-	input 	logic	[2:0]		F,
+	input	logic	[31:0]	A, B,
+	input 	logic	[2:0]	F,
 	output 	logic	[31:0]	Y,
-	output	logic				zero);
+	output	logic		zero, OF);
 
 	logic [31:0] BB, aluAND, aluOR, aluSUM, aluZSUM;
-	
+	logic overflow;
 	
 	assign BB = F[2] ? ~B : B;
 	assign aluAND = A & BB;
 	assign aluOR = A | BB;
 	assign aluSUM = A + BB + F[2];
 	assign aluZSUM = {31'b0, aluSUM[31]};
+	assign overflow = ~(A[31] ^ BB[31]) & (aluSUM[31] ^ A[31]);
 	
 	always_comb
 	begin
@@ -27,9 +28,11 @@ module alu(
 		if (Y == 32'd0) begin
 			zero = 1'b1;
 		end
-	end
-	
-	
 
+		OF = 1'b0;
+		if (F == 3'b010 || F == 3'110) begin
+			OF = overflow;
+		end
+	end
 
 endmodule
